d5a2bb61cde9840c43c17a3ddf81a6a813528d2f
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-05-23 12:52:30 +0100

    Update styled components and add basic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-function App() {
+import Contact from "./components/Contact";
+import Hero from "./components/Hero";
+import Who from "./components/Who";
+import Work from "./components/Work";
+import styled from "styled-components";
 
-    return (
-      <div>
-        Hello world!
-      </div>
-    )
-  }
-  
-  export default App
-  
+const Container = styled.div`
+  height: 100vh;
+  background-color: purple;
+`
+
+function App() {
+  return (
+    <Container>
+      <Hero />
+      <Who />
+      <Work />
+      <Contact />
+    </Container>
+  );
+}
+
+export default App;
